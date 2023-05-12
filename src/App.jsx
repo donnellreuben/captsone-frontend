b733b88d98a5dd1433f5e41f3c9f269708acc429
Donnell Reuben <donnell.reuben@icloud.com>
2023-05-12 11:48:49 -0500

    hardcoded html, good template, will organize next

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Header() {
+    return (
+      <header>
+        <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+      </header>
+    );
 }
 
-export default App
+function ProductsNew() {
+    return (
+      <div id="posts-new">
+        <h1>New post</h1>
+      </div>
+    );
+}
+
+function ProductsIndex() {
+    return ( 
+      <div id="posts-index">
+        <h1>All posts</h1>
+      </div>
+    );
+}
+
+function Footer() {
+    return (
+      <footer>
+        <p>Copyright 2023</p>
+      </footer>
+    );
+}
+
+function Content() {
+  return (
+    <div>
+      <ProductsNew />
+      <ProductsIndex />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
